e368650d3afeeb7ac8044ad4d8bab6f1b22c3a03
Alejandro Coca <alxmcr@outlook.com>
2025-06-03 19:01:41 -0400

    wip

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import politicalPartiesRoutes from "./routes/political-parties.routes";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/political-parties", politicalPartiesRoutes);
 
