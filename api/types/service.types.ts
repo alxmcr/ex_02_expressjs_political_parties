bffba1f14814403847e2b625ebacc21bf744729e
Alejandro Coca <alxmcr@outlook.com>
2025-06-04 02:09:53 -0400

    wip

--- api/types/service.types.ts
+++ api/types/service.types.ts
@@ -2,12 +2,6 @@ export interface PoliticalParty {
   id: number;
   name: string;
   abbreviation: string;
-  founded_date: string; // still a string if coming from JSON, though it's a Date in DB
-  leader: string;
-  headquarters: string;
-  website: string;
-  number_of_members: number;
-  ballot_status: string;
   presidential_candidate: string;
   vice_presidential_candidate: string;
   created_at: string; // ISO timestamp string
