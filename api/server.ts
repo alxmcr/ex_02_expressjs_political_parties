49cdd2cd9c86db7dbdce4ed63d1f9e1ef1a8c60a
Alejandro Coca <alxmcr@outlook.com>
2025-06-09 00:23:14 -0400

    wip

--- api/server.ts
+++ api/server.ts
@@ -3,7 +3,13 @@ import cors from "cors";
 import morgan from "morgan";
 import politicalPartiesRoutes from "./routes/political-parties.routes";
 import swaggerRoutes from "./routes/swagger.routes";
+import statusRoutes from "./routes/status.routes";
+
+// Get the host machine's IP address like when we deploy to a server
+const host = process.env.HOST || "localhost";
+// Port from environment variables or default to 3000
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.json());
@@ -12,10 +18,17 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 
+app.get("/", (req, res) => {
+  res.json({
+    status: "Welcome to the API!",
+    docs: `http://${host}:${PORT}/api/swagger`,
+  });
+});
+app.use("/api/status", statusRoutes);
 app.use("/api/political-parties", politicalPartiesRoutes);
 app.use("/api/swagger", swaggerRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`Swagger is running on http://localhost:${PORT}/api/swagger`);
+  console.log(`Swagger is running on http://${host}:${PORT}/api/swagger`);
 });
